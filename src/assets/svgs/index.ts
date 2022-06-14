0ba229b7fea84bbf48a634ab6152fec16309fcf2
zellytozelly <zellytozelly@gmail.com>
2022-06-14 19:31:51 +0900

    Feat: Add icons

--- src/assets/svgs/index.ts
+++ src/assets/svgs/index.ts
@@ -1,1 +1,3 @@
 export { ReactComponent as ThinSearchIcon } from './search-thin.svg'
+export { ReactComponent as BallIcon } from './ball.svg'
+export { ReactComponent as ArrowLeftIcon } from './arrow-left.svg'
