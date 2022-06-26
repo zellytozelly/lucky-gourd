fd788a22a8cd43b92c9e2e4e14c41f4b07ee7f57
zellytozelly <zellytozelly@gmail.com>
2022-06-26 21:41:25 +0900

    Fix: Fix nelify error

--- src/routes/Main/index.tsx
+++ src/routes/Main/index.tsx
@@ -1,7 +1,6 @@
 import { Link } from 'react-router-dom'
 
 import BasicGourd from 'assets/images/mainGourd.png'
-import { BallIcon } from 'assets/svgs'
 import styles from './main.module.scss'
 
 const Main = () => {
